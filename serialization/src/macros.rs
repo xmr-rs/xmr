69c2b71af4e5a43d60ba7f2f4e733eb7f1b720f2
Jean Pierre Dudey <jeandudey@hotmail.com>
2018-02-06 11:54:36 -0400

    Add docuentation for `serialize2!` macro.
    Also i've changed some of the macro internals to allow deserialization
    in some near future.
    
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

--- serialization/src/macros.rs
+++ serialization/src/macros.rs
@@ -1,35 +1,72 @@
+// Greensputh's tenth rule anywhere?
+
 // TODO: Improve macro syntax
+/// A macro to craft `Serialize` and `Deserialize` routines for an structure.
+///
+/// # Usage:
+///
+/// `serialize!2 { field -> (specifier), ...,  }`
+///
+/// `specifier` should be one of the valid identifiers found below.
+///
+/// # Notes:
+///
+/// The field list should end with a trailing comma.
+///
+/// # Example:
+///
+/// ```rust
+/// #[derive(Debug)]
+/// pub struct TransactionHistory {
+///     pub id: u32,
+///     pub extra: u8,
+/// }
+/// 
+/// serialize2! {
+///     TransactionHistory {
+///         id -> (num),
+///         // note the trailing comma.
+///         extra -> (num),
+///     }
+/// }
+/// ```
+///
+/// ## Specifier list
+///
+/// - `num`: a number it can be signed or unsigned.
+/// - `varint`: a signed variable-length integer.
+/// - ``
 #[macro_export]
 macro_rules! serialize2 {
     ($struct_name:ident { $( $name:ident -> ($($discriminator:tt)*) ,)* }) => {
         impl $crate::serializer::Serialize for $struct_name {
             fn serialize<T: $crate::serializer::Serializer>(&self, serializer: &mut T) {
                 $(
-                    serialize2!(__internal self, serializer, $name -> $($discriminator)*);
+                    serialize2!(__serialize self, serializer, $name -> $($discriminator)*);
                 )*
             }
         }
     };
-    (__internal $self:ident, $serializer:ident, $field_name:ident -> num) => {
+    (__serialize $self:ident, $serializer:ident, $field_name:ident -> num) => {
         {
             $serializer.tag(stringify!($field_name));
             $serializer.serialize_num($self.$field_name);
         }
     };
-    (__internal $self:ident, $serializer:ident, $field_name:ident -> varint) => {
+    (__serialize $self:ident, $serializer:ident, $field_name:ident -> varint) => {
         {
             $serializer.tag(stringify!($field_name));
             $serializer.serialize_varint($self.$field_name);
         }
     };
-    (__internal $self:ident, $serializer:ident, $field_name:ident -> uvarint) => {
+    (__serialize $self:ident, $serializer:ident, $field_name:ident -> uvarint) => {
         {
             $serializer.tag(stringify!($field_name));
             $serializer.serialize_uvarint($self.$field_name);
         }
     };
 
-    (__internal $self:ident, $serializer:ident, $field_name:ident -> blob) => {
+    (__serialize $self:ident, $serializer:ident, $field_name:ident -> blob) => {
         {
             $serializer.tag(stringify!($field_name));
             $serializer.serialize_blob(&$self.$field_name);
