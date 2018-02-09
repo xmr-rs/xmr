e350f194bf60d0a6cec29e09f12a5b717f3561f3
Jean Pierre Dudey <jeandudey@hotmail.com>
2018-02-09 10:35:08 -0400

    Add `peers` configuration
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

--- p2p/src/config.rs
+++ p2p/src/config.rs
@@ -1,7 +1,13 @@
+use std::net::SocketAddr;
 use uuid::Uuid;
 
+/// P2P configuration.
 #[derive(Debug, Clone)]
 pub struct Config {
+    /// Number of threads.
     pub threads: usize,
+    /// The network id.
     pub network_id: Uuid,
+    /// Peers to connect.
+    pub peers: Vec<SocketAddr>,
 }
