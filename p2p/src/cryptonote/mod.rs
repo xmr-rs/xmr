35c75e976af50291d9817d356d76cfdcc750694f
Jean Pierre Dudey <jeandudey@hotmail.com>
2018-02-12 17:50:13 -0400

    Add doc-strings for `CoreSyncData` strucutre.
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

--- p2p/src/cryptonote/mod.rs
+++ p2p/src/cryptonote/mod.rs
@@ -12,8 +12,11 @@ use hash::H256;
 pub struct CoreSyncData {
     /// The current block height
     pub current_height: u64,
+    /// The cumulative difficulty.
     pub cumulative_difficulty: u64,
+    /// The top block id.
     pub top_id: H256,
+    /// The top block version.
     pub top_version: u8,
 }
 
