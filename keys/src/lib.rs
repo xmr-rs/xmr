b6aa1fc2071b6a924120bd6fd5baf1d246b407c3
Jean Pierre Dudey <jeandudey@hotmail.com>
2018-02-13 21:48:44 -0400

    Add `rct` and `keys` crates.
    For now they are only placeholder crates. Functionality is going to be added.
    
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

---
+++ keys/src/lib.rs
@@ -0,0 +1,11 @@
+#[derive(Debug, Default, Clone)]
+pub struct KeyImage;
+
+#[derive(Debug, Default, Clone)]
+pub struct Signature;
+
+#[derive(Debug, Default, Clone)]
+pub struct PublicKey;
+
+#[derive(Debug, Default, Clone)]
+pub struct SecretKey;
