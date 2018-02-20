af07af4699472eeaf406d9ce048419abd65afae6
Jean Pierre Dudey <jeandudey@hotmail.com>
2018-02-20 12:15:50 -0400

    Add p2p support flags constants.
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

--- p2p/src/config.rs
+++ p2p/src/config.rs
@@ -15,3 +15,6 @@ pub struct Config {
     /// Hide my port.
     pub hide_my_port: bool,
 }
+
+pub const P2P_SUPPORT_FLAG_FLUFFY_BLOCKS: u32 = 0x01;
+pub const P2P_SUPPORT_FLAGS: u32 = P2P_SUPPORT_FLAG_FLUFFY_BLOCKS;
