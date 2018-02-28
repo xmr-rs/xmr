6935c520deb451055deecad43287da4ae7604611
Jean Pierre Dudey <jeandudey@hotmail.com>
2018-02-28 13:55:32 -0400

    Add method to calculate transaction tree hash.
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

--- hash/src/lib.rs
+++ hash/src/lib.rs
@@ -24,6 +24,83 @@ impl H256 {
         H256(slow_hash(input.as_ref()))
     }
 
+    fn tree_hash_cnt(count: usize) -> usize {
+        assert!(count >= 3);
+        assert!(count <= 0x10000000);
+
+        let mut pow = 2;
+        while pow < count {
+            pow <<= 1;
+        }
+        pow >> 1
+    }
+
+    pub fn tree_hash<T>(hashes: T) -> H256 where T: AsRef<[H256]> {
+        let hashes = hashes.as_ref();
+        assert!(hashes.len() > 0);
+
+        match hashes.len() {
+            0 => panic!("tree hash needs at least one element"),
+            1 => hashes[0].clone(),
+            2 => {
+                let mut buf = [0u8; H256_LENGTH * 2];
+                (&mut buf[..H256_LENGTH]).copy_from_slice(hashes[0].as_bytes());
+                (&mut buf[H256_LENGTH..]).copy_from_slice(hashes[1].as_bytes());
+
+                H256(fast_hash(&buf))
+            },
+            count => {
+                let cnt = Self::tree_hash_cnt(count);
+                let mut ints = Vec::with_capacity(cnt);
+                for _ in 0..cnt {
+                    ints.push(H256::new());
+                }
+
+                for i in 0..(2 * cnt - count) {
+                    ints[i] = hashes[i].clone();
+                }
+
+                let mut i = 2 * cnt - count;
+                let mut j = 2 * cnt - count;
+
+                while j < cnt {
+                    let mut buf = [0u8; H256_LENGTH * 2];
+                    (&mut buf[..H256_LENGTH]).copy_from_slice(hashes[i].as_bytes());
+                    (&mut buf[H256_LENGTH..]).copy_from_slice(hashes[i + 1].as_bytes());
+
+                    ints[j] = H256(fast_hash(&buf));
+                    i += 2;
+                    j += 1;
+                }
+
+                assert!(i == count);
+
+                let mut cnt = cnt;
+                while cnt > 2 {
+                    cnt >>= 1;
+
+                    let mut i = 0;
+                    let mut j = 0;
+                    while j < cnt {
+                        let mut buf = [0u8; H256_LENGTH * 2];
+                        (&mut buf[..H256_LENGTH]).copy_from_slice(ints[i].as_bytes());
+                        (&mut buf[H256_LENGTH..]).copy_from_slice(ints[i + 1].as_bytes());
+
+                        ints[j] = H256(fast_hash(&buf));
+                        i += 2;
+                        j += 1;
+                    }
+                }
+
+                let mut buf = [0u8; H256_LENGTH * 2];
+                (&mut buf[..H256_LENGTH]).copy_from_slice(ints[0].as_bytes());
+                (&mut buf[H256_LENGTH..]).copy_from_slice(ints[1].as_bytes());
+
+                H256(fast_hash(&buf))
+            }
+        }
+    }
+
     pub fn from_bytes<B: AsRef<[u8]>>(bytes: B) -> H256 {
         let bytes = bytes.as_ref();
         assert!(bytes.len() == H256_LENGTH, "invalid hash length");
