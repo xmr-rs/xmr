aa7773d7c3a9e0f8af52c78634681b602aa222d4
Jean Pierre Dudey <jeandudey@hotmail.com>
2018-03-10 20:56:16 -0400

    Remove `Invoke` future.
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

--- p2p/src/levin/mod.rs
+++ p2p/src/levin/mod.rs
@@ -1,6 +1,5 @@
 mod bucket;
 mod command;
-mod invoke;
 mod notify;
 mod receive;
 mod response;
@@ -8,7 +7,6 @@ mod result;
 
 pub use self::bucket::{BUCKET_HEAD_LENGTH, Bucket, BucketHead, invoke_bucket, response_bucket, notify_bucket};
 pub use self::command::{COMMAND_BASE_ID, Command, Notify, Storage, Empty};
-pub use self::invoke::{Invoke, invoke};
 pub use self::notify::{NotifyFuture, notify};
 pub use self::receive::{Receive, receive};
 pub use self::response::{Response, response};
